8da53510b4393b1b8b814e907f211a0cc95c0c58
malbong <gon3578@gmail.com>
2021-06-16 07:40:17 +0900

    fixed typo error

--- merge/include/Response.hpp
+++ merge/include/Response.hpp
@@ -14,7 +14,7 @@ public:
 	Response(const Response &ref);
 	~Response(void);
 	Response	&operator=(const Response &ref);
-	Location	getLocation(const Server& server, const std::string& uri);
+	Location	getMatchingLocation(const Server& server, const std::string& uri);
 	std::string	getRealPath(const Location& location, const std::string& uri);
 	std::string	isAllowedMethod(const Location& location, const std::string& method);
 	void		response(const Server& server, const Request& request);
